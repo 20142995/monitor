d44233657cace6b282265a2d3ccfa26c77775d2f
test <test>
2022-11-18 11:57:14 +0800

    1

--- service_monitoring.py
+++ service_monitoring.py
@@ -6,6 +6,7 @@ import requests
 DINGTALK_TOKEN = os.getenv('DINGTALK_TOKEN')
 DDDDOCR_URL = os.getenv('DDDDOCR_URL')
 
+
 def send_text(text, token):
     headers = {'Content-Type': 'application/json'}
     data = {"msgtype": "text", "text": {"content": text},
@@ -13,6 +14,7 @@ def send_text(text, token):
     url = "https://oapi.dingtalk.com/robot/send?access_token={}".format(token)
     r = requests.post(url, json=data, headers=headers)
     return r.json()
+
 
 def check_ddddocr():
     try:
@@ -23,10 +25,9 @@ def check_ddddocr():
         pass
     return False
 
+
 if __name__ == '__main__':
     text = '服务监控:'
-    text += '\n{}\t{}\t{}'.format("验证码识别接口",DDDDOCR_URL,'正常' if check_ddddocr() else '异常')
+    text += '\n{}\t{}\t{}'.format("验证码识别接口", DDDDOCR_URL,
+                                  '正常' if check_ddddocr() else '异常')
     send_text(text, DINGTALK_TOKEN)
-
-
-
